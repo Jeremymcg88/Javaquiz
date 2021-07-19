438eda18afd17c050a89d1e542623c88180b2062
Jeremymcg88 <jeremy.mcguirk@gmail.com>
2021-07-19 19:12:55 -0400

    added java. still trying to figure out how to add in the timer, score, and save score to local database.

--- assets/javascript/questions.js
+++ assets/javascript/questions.js
@@ -4,5 +4,9 @@ var myQuestions = [
         multiChoice: [ ".numbers and strings", ".other arrays", ".booleans", ".all of the above"],
         answer: ".all of the above"
     },
-    
+    {
+        question: "Commonly used data types DO NOT include",
+        multiChoice: [ ".strings", ".booleans", ".alerts", ".numbers"],
+        answer: ".alerts"
+    },
 ]
