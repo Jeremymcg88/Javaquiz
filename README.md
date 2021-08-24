d863c6a66d764a167499562950b0499b283419ea
Jeremymcg88 <jeremy.mcguirk@gmail.com>
2021-08-24 03:50:32 -0400

    added the JS. Works in conjunction with the HTML

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Javaquiz
+# Javaquiz 
--- assets/javascript/questions.js
+++ assets/javascript/questions.js
@@ -1,12 +1,31 @@
-var myQuestions = [
+var questions = [
     {
-        question: "Arrays in Javascript can be used to store ________.",
-        multiChoice: [ ".numbers and strings", ".other arrays", ".booleans", ".all of the above"],
-        answer: ".all of the above"
+      title: "An array’s length can be evaluated with the what property?",
+      multiChoice: [".length", ".log", "the console", ".loop"],
+      answer: ".length"
     },
+  
     {
-        question: "Commonly used data types DO NOT include",
-        multiChoice: [ ".strings", ".booleans", ".alerts", ".numbers"],
-        answer: ".alerts"
+      title: "Within a loop, the 'break' keyword may be used to do what?",
+      multiChoice: ["break your competitors code", "exit the loop immediately", "repeat the loop", "indicate a stopping condition"],
+      answer: "exit the loop immediately"
     },
-]
+  
+    {
+      title: "Properties in a JavaScript oject are often refferred to as what?",
+      multiChoice: ["dot walking", "key-value pairs", "nested properties", "undefined"],
+      answer: "key-value pairs"
+    },
+  
+    {
+      title: "Which array method inserts an element at the end of the array?",
+      multiChoice: [".pop()", ".push()", ".length", ".join()"],
+      answer: ".push()"
+    },
+  
+    {
+      title: "What is a callback function?",
+      multiChoice: ["a function that accepts an array as an argument", "I function that performs an HTTP request", "a data type similar to a string or a boolean", "a function that is passed into another function as an argument"],
+      answer: "a function that is passed into another function as an argument"
+    }
+  ];
